2c6040eb2a56f1123a6017c88a2a9779d9108609
deeRocking <deelayney@yahoo.com>
2025-01-04 13:06:38 +0100

    initial commit, defined the project file system

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import math
+from typing import Tuple
+
+# noinspection PyUnresolvedReferences
+import vtkmodules.vtkInteractionStyle
+# noinspection PyUnresolvedReferences
+import vtkmodules.vtkRenderingOpenGL2
+from vtkmodules.vtkCommonColor import vtkNamedColors
+from vtkmodules.vtkFiltersCore import vtkGlyph3D
+from vtkmodules.vtkFiltersSources import vtkArrowSource
+from vtkmodules.vtkCommonCore import (
+    vtkDoubleArray,
+    vtkMath,
+    vtkPoints
+)
+from vtkmodules.vtkCommonDataModel import vtkStructuredPoints
+from vtkmodules.vtkRenderingCore import (
+    vtkActor,
+    vtkPolyDataMapper,
+    vtkRenderWindow,
+    vtkRenderWindowInteractor,
+    vtkRenderer
+)
+
+
+def main() -> None:
+    pass
+
